443a05e8a607eded8fb948d43207b7ca9d396267
HP <HP@host.docker.internal>
2023-08-31 02:00:13 +0530

    change 1

--- deom/src/main/java/com/plugsity/com/Controller.java
+++ deom/src/main/java/com/plugsity/com/Controller.java
@@ -1,6 +1,6 @@
 package com.plugsity.com;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/plugisty/avi/v1/")
 public class Controller {
 
-	@PostMapping("/saveBusinessUser")
+	@GetMapping("/saveBusinessUser")
 	public String saveBusinessUser() {
 		 
 		return "hello";
